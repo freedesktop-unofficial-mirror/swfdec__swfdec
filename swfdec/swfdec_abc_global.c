4b8d2e47c997f09adba04d98139f819440dd3237
Benjamin Otte <otte@gnome.org>
2008-08-07 19:19:50 +0100

    add missing files

---
+++ swfdec/swfdec_abc_global.c
@@ -0,0 +1,135 @@
+/* Swfdec
+ * Copyright (C) 2007 Benjamin Otte <otte@gnome.org>
+ *               2007 Pekka Lampila <pekka.lampila@iki.fi>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "swfdec_abc_global.h"
+#include "swfdec_as_context.h"
+#include "swfdec_as_frame_internal.h"
+#include "swfdec_as_function.h"
+#include "swfdec_as_object.h"
+#include "swfdec_as_strings.h"
+#include "swfdec_as_internal.h"
+#include "swfdec_as_native_function.h"
+#include "swfdec_system.h"
+#include "swfdec_player_internal.h"
+#include "swfdec_debug.h"
+
+G_DEFINE_TYPE (SwfdecAbcGlobal, swfdec_abc_global, SWFDEC_TYPE_AS_OBJECT)
+
+static void
+swfdec_abc_global_dispose (GObject *object)
+{
+  SwfdecAbcGlobal *global = SWFDEC_ABC_GLOBAL (object);
+
+  g_ptr_array_free (global->traits, TRUE);
+
+  G_OBJECT_CLASS (swfdec_abc_global_parent_class)->dispose (object);
+}
+
+static void
+swfdec_abc_global_mark (SwfdecGcObject *object)
+{
+  SwfdecAbcGlobal *global = SWFDEC_ABC_GLOBAL (object);
+
+  g_ptr_array_foreach (global->traits, (GFunc) swfdec_gc_object_mark, NULL);
+
+  SWFDEC_GC_OBJECT_CLASS (swfdec_abc_global_parent_class)->mark (object);
+}
+
+static void
+swfdec_abc_global_class_init (SwfdecAbcGlobalClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  SwfdecGcObjectClass *gc_class = SWFDEC_GC_OBJECT_CLASS (klass);
+
+  object_class->dispose = swfdec_abc_global_dispose;
+
+  gc_class->mark = swfdec_abc_global_mark;
+}
+
+static void
+swfdec_abc_global_init (SwfdecAbcGlobal *global)
+{
+  global->traits = g_ptr_array_new ();
+}
+
+void
+swfdec_abc_global_add_traits (SwfdecAbcGlobal *global, SwfdecAbcTraits *traits)
+{
+  g_return_if_fail (SWFDEC_IS_ABC_GLOBAL (global));
+  g_return_if_fail (SWFDEC_IS_ABC_TRAITS (traits));
+  g_return_if_fail (swfdec_abc_global_get_traits (global, traits->ns, traits->name) == NULL);
+
+  g_ptr_array_add (global->traits, traits);
+}
+
+SwfdecAbcTraits *
+swfdec_abc_global_get_traits (SwfdecAbcGlobal *global, SwfdecAbcNamespace *ns,
+    const char *name)
+{
+  guint i;
+
+  g_return_val_if_fail (SWFDEC_IS_ABC_GLOBAL (global), NULL);
+  g_return_val_if_fail (SWFDEC_IS_ABC_NAMESPACE (ns), NULL);
+  g_return_val_if_fail (name != NULL, NULL);
+
+  for (i = 0; i < global->traits->len; i++) {
+    SwfdecAbcTraits *traits = g_ptr_array_index (global->traits, i);
+    if (traits->ns == ns && traits->name == name)
+      return traits;
+  }
+  return NULL;
+}
+
+SwfdecAbcTraits *
+swfdec_abc_global_get_traits_for_multiname (SwfdecAbcGlobal *global,
+    const SwfdecAbcMultiname *mn)
+{
+  SwfdecAbcTraits *ret, *tmp;
+  guint i;
+
+  g_return_val_if_fail (SWFDEC_IS_ABC_GLOBAL (global), NULL);
+  g_return_val_if_fail (mn != NULL, NULL);
+
+  if (!swfdec_abc_multiname_is_binding (mn))
+    return NULL;
+
+  ret = NULL;
+  for (i = 0; i < swfdec_abc_multiname_get_n_namespaces (mn); i++) {
+    tmp = swfdec_abc_global_get_traits (global, 
+	swfdec_abc_multiname_get_namespace (mn, i), mn->name);
+    if (tmp) {
+      if (ret == NULL) {
+	ret = tmp;
+      } else {
+	SWFDEC_FIXME ("throw ambigious namespace error here");
+	return NULL;
+      }
+    }
+  }
+  return ret;
+}
---
+++ swfdec/swfdec_abc_global.h
@@ -0,0 +1,62 @@
+/* Swfdec
+ * Copyright (C) 2008 Benjamin Otte <otte@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ */
+
+#ifndef _SWFDEC_ABC_GLOBAL_H_
+#define _SWFDEC_ABC_GLOBAL_H_
+
+#include <swfdec/swfdec_as_object.h>
+#include <swfdec/swfdec_abc_multiname.h>
+#include <swfdec/swfdec_abc_traits.h>
+
+G_BEGIN_DECLS
+
+typedef struct _SwfdecAbcGlobal SwfdecAbcGlobal;
+typedef struct _SwfdecAbcGlobalClass SwfdecAbcGlobalClass;
+
+#define SWFDEC_TYPE_ABC_GLOBAL                    (swfdec_abc_global_get_type())
+#define SWFDEC_IS_ABC_GLOBAL(obj)                 (G_TYPE_CHECK_INSTANCE_TYPE ((obj), SWFDEC_TYPE_ABC_GLOBAL))
+#define SWFDEC_IS_ABC_GLOBAL_CLASS(klass)         (G_TYPE_CHECK_CLASS_TYPE ((klass), SWFDEC_TYPE_ABC_GLOBAL))
+#define SWFDEC_ABC_GLOBAL(obj)                    (G_TYPE_CHECK_INSTANCE_CAST ((obj), SWFDEC_TYPE_ABC_GLOBAL, SwfdecAbcGlobal))
+#define SWFDEC_ABC_GLOBAL_CLASS(klass)            (G_TYPE_CHECK_CLASS_CAST ((klass), SWFDEC_TYPE_ABC_GLOBAL, SwfdecAbcGlobalClass))
+#define SWFDEC_ABC_GLOBAL_GET_CLASS(obj)          (G_TYPE_INSTANCE_GET_CLASS ((obj), SWFDEC_TYPE_ABC_GLOBAL, SwfdecAbcGlobalClass))
+
+struct _SwfdecAbcGlobal {
+  SwfdecAsObject	object;
+
+  GPtrArray *		traits;		/* list of all traits - FIXME: needs fast index by ns/name */
+};
+
+struct _SwfdecAbcGlobalClass {
+  SwfdecAsObjectClass	object_class;
+};
+
+GType			swfdec_abc_global_get_type	(void);
+
+void			swfdec_abc_global_add_traits	(SwfdecAbcGlobal *		global,
+							 SwfdecAbcTraits *		traits);
+SwfdecAbcTraits *	swfdec_abc_global_get_traits	(SwfdecAbcGlobal *		global,
+							 SwfdecAbcNamespace *		ns,
+							 const char *			name);
+SwfdecAbcTraits *	swfdec_abc_global_get_traits_for_multiname
+							(SwfdecAbcGlobal *		global,
+							 const SwfdecAbcMultiname *	mn);
+
+
+G_END_DECLS
+#endif
