7bf845395a83875164e92294aa380676215378f9
Benjamin Otte <otte@gnome.org>
2008-12-08 11:37:57 +0100

    add a SwfdecRtmpPacket type in preparation for further changes

---
+++ swfdec/swfdec_rtmp_packet.c
@@ -0,0 +1,51 @@
+/* Swfdec
+ * Copyright (C) 2008 Benjamin Otte <otte@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "swfdec_rtmp_packet.h"
+
+SwfdecRtmpPacket *
+swfdec_rtmp_packet_new (SwfdecRtmpPacketType type, guint timestamp,
+    SwfdecBuffer *buffer)
+{
+  SwfdecRtmpPacket *packet;
+
+  g_return_val_if_fail (buffer != NULL, NULL);
+
+  packet = g_slice_new0 (SwfdecRtmpPacket);
+  packet->type = type;
+  packet->timestamp = timestamp;
+  packet->buffer = swfdec_buffer_ref (buffer);
+
+  return packet;
+}
+
+void
+swfdec_rtmp_packet_free (SwfdecRtmpPacket *packet)
+{
+  g_return_if_fail (packet != NULL);
+
+  swfdec_buffer_unref (packet->buffer);
+  g_slice_free (SwfdecRtmpPacket, packet);
+}
+
+
---
+++ swfdec/swfdec_rtmp_packet.h
@@ -0,0 +1,44 @@
+/* Swfdec
+ * Copyright (C) 2008 Benjamin Otte <otte@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ */
+
+#ifndef _SWFDEC_RTMP_PACKET_H_
+#define _SWFDEC_RTMP_PACKET_H_
+
+#include <swfdec/swfdec.h>
+#include <swfdec/swfdec_rtmp_header.h>
+
+G_BEGIN_DECLS
+
+
+typedef struct _SwfdecRtmpPacket SwfdecRtmpPacket;
+
+struct _SwfdecRtmpPacket {
+  SwfdecRtmpPacketType	type;		/* type of packet */
+  guint			timestamp;	/* timestamp associated with this packet */
+  SwfdecBuffer *	buffer;		/* contents of packet */
+};
+
+SwfdecRtmpPacket *	swfdec_rtmp_packet_new		(SwfdecRtmpPacketType		type,
+							 guint				timestamp,
+							 SwfdecBuffer *			buffer);
+void			swfdec_rtmp_packet_free		(SwfdecRtmpPacket *		packet);
+
+
+G_END_DECLS
+#endif
